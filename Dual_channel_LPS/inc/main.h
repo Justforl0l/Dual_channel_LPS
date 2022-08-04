e5eb31d84c8fd68b10e14cde82e41f8d509f12f1
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-08-04 20:32:56 +0400

    Первый коммит

---
+++ Dual_channel_LPS/inc/main.h
@@ -0,0 +1,16 @@
+/*
+ * main.h
+ *
+ * Created: 04.08.2022 20:25:44
+ *  Author: shama
+ */ 
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <avr/io.h>
+
+
+
+#endif /* MAIN_H_ */
---
+++ Dual_channel_LPS/src/main.c
@@ -0,0 +1,17 @@
+/*
+ * Dual_channel_LPS.c
+ *
+ * Created: 04.08.2022 20:25:05
+ * Author : shama
+ */ 
+
+#include "../inc/main.h"
+
+
+int main(void)
+{
+    while (1) 
+    {
+    }
+}
+
