764b857796d1b81f1ac007d425fc918cc5622ae1
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-08-06 14:05:27 +0400

    Добавил модуль util для тактовой кнопки
    - Добавил объявление функции button_is_pressed в util.h
    - Добавил объявление функции toggle_output_load в util.h
    - Добавил пустую реализацию функции button_is_pressed в util.c
    - Добавил пустую реализацию функции toggle_output_load в util.c

---
+++ Dual_channel_LPS/inc/button/util.h
@@ -0,0 +1,17 @@
+/*
+ * util.h
+ *
+ * Created: 06.08.2022 13:52:52
+ *  Author: shama
+ */ 
+
+
+#ifndef BUTTON_UTIL_H_
+#define BUTTON_UTIL_H_
+
+#include <avr/io.h>
+
+bool button_is_pressed(void);
+void toggle_output_load(void);
+
+#endif /* BUTTON_UTIL_H_ */
---
+++ Dual_channel_LPS/src/button/util.c
@@ -0,0 +1,18 @@
+/*
+ * util.c
+ *
+ * Created: 06.08.2022 13:51:26
+ *  Author: shama
+ */ 
+
+#include "button/util.h"
+
+bool button_is_pressed(void)
+{
+	return;
+}
+
+void toggle_output_load(void)
+{
+	return;
+}
