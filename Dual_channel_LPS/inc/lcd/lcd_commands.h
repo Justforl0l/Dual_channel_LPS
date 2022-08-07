7896f60f63c05e96b334cbbe49857b8e97fd8d12
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-08-07 16:21:59 +0400

    Определил макросы для команд и параметров дисплея

---
+++ Dual_channel_LPS/inc/lcd/lcd_commands.h
@@ -0,0 +1,19 @@
+/*
+ * lcd_commands.h
+ *
+ * Created: 07.08.2022 16:00:59
+ *  Author: shama
+ */ 
+
+
+#ifndef LCD_COMMANDS_H_
+#define LCD_COMMANDS_H_
+
+#define LCD_CMD_CLEAR_DISPLAY		0x01
+#define LCD_CMD_RETURN_HOME			0x02
+#define LCD_CMD_ENTRY_MODE_SET		0x04
+#define LCD_CMD_DISPLAY_OFF			0x08
+#define LCD_CMD_DISPLAY_ON			0x0C
+#define LCD_CMD_FUNCTION_SET		0x28
+
+#endif /* LCD_COMMANDS_H_ */
