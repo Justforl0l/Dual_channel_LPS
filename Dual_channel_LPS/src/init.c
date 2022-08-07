afb3d26a3629b14533826dc31bd72821634aa6f1
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-08-07 17:16:25 +0400

    Начал реализацию функции lcd_init по даташиту (не закончил)

--- Dual_channel_LPS/src/init.c
+++ Dual_channel_LPS/src/init.c
@@ -26,20 +26,13 @@ void lcd_init(void)
 {
 	DDRD = 0xFF;
 	PORTD = 0x00;
-	_delay_ms(50);
-	
-	/* Конфигурирование четырёхразрядного режима */
-	PORTD |= (1 << PD5);
-	PORTD &= ~(1 << PD4);
-	
-	/* Активация четырёхразрядного режима */
-	PORTD |= (1 << EN);
-	PORTD &= ~(1 << EN);
+	_delay_ms(20);
+	lcd_send_command((LCD_CMD_FUNCTION_SET | LCD_SETTING_DATA_LENGTH_8_BIT), _8_BIT_MODE);
 	_delay_ms(5);
-	lcd_send_command(0x28);		/* Шина 4 бит, LCD - 2 строки */
-	lcd_send_command(0x08);		/* Полное выключение дисплея */
-	lcd_send_command(0x01);		/* Очистка дисплея */
-	_delay_us(100);
-	lcd_send_command(0x06);		/* Сдвиг курсора вправо */
-	lcd_send_command(0x0C);		/* Включение дисплея, курсор не видим */
+	lcd_send_command((LCD_CMD_FUNCTION_SET | LCD_SETTING_DATA_LENGTH_8_BIT), _8_BIT_MODE);
+	_delay_us(110);
+	lcd_send_command((LCD_CMD_FUNCTION_SET | LCD_SETTING_DATA_LENGTH_8_BIT), _8_BIT_MODE);
+	
+	lcd_send_command((LCD_CMD_FUNCTION_SET | LCD_SETTING_DATA_LENGTH_4_BIT), _8_BIT_MODE);
+	/* TODO: Завершить код инициализации дисплея */
 }
