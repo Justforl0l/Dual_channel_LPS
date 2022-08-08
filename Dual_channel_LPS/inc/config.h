a6722c695388be80f2e6d6dde963a890e049ed8c
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-08-08 09:35:42 +0400

    Определил макросы для необходимых выводов

---
+++ Dual_channel_LPS/inc/config.h
@@ -0,0 +1,20 @@
+/*
+ * config.h
+ *
+ * Created: 08.08.2022 9:30:18
+ *  Author: shama
+ */ 
+
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+/* ”правл€ющие выводы LCD диспле€ */
+#define RS					PD0
+#define RW					PD1
+#define EN					PD2
+
+#define BUTTON_PIN			PB1
+#define LOAD_TOGGLER		PB0
+
+#endif /* CONFIG_H_ */
