019767546571696e7af1ca6100e8f5d49dea267f
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-08-08 11:25:53 +0400

    Рефактор и доработка модуля lcd_util
    - Изменил сигнатуру функции lcd_send_command
    - Добавил макро определение опций проверки busy flag
    - Добавил включение config.h

--- Dual_channel_LPS/inc/lcd/lcd_util.h
+++ Dual_channel_LPS/inc/lcd/lcd_util.h
@@ -10,14 +10,22 @@
 #define LCD_UTIL_H_
 
 #include <avr/io.h>
+#include <stdbool.h>
+
+#define F_CPU 8000000
+#include <util/delay.h>
 
 #include "config.h"
 
-#define _8_BIT_MODE			1
-#define _4_BIT_MODE			0
+#define _8_BIT_MODE						1
+#define _4_BIT_MODE						0
+#define CHECK_BUSY_FLAG					1
+#define DONT_CHECK_BUSY_FLAG			0
 
-void lcd_send_command(unsigned char command, unsigned char mode);
+void lcd_send_command(unsigned char command, unsigned char mode,
+					  unsigned char check_busy_flag);
 void lcd_send_data(unsigned char data);
 void lcd_display_string(unsigned char command, char *string);
+bool lcd_is_busy(void);
 
 #endif /* LCD_UTIL_H_ */
--- Dual_channel_LPS/src/lcd/lcd_util.c
+++ Dual_channel_LPS/src/lcd/lcd_util.c
@@ -7,9 +7,30 @@
 
 #include "lcd/lcd_util.h"
 
-void lcd_send_command(unsigned char command, unsigned char mode)
+void lcd_send_command(unsigned char command, unsigned char mode,
+					  unsigned char check_busy_flag)
 {
-	return;
+	LCD_PORT &= ~((1 << RS) | (1 << RW));
+	_delay_us(1);
+	
+	/* Старший нибл */
+	LCD_PORT |= (1 << EN);
+	LCD_PORT &= 0x0F;
+	LCD_PORT |= (command & 0xF0);
+	_delay_us(1);
+	LCD_PORT &= ~(1 << EN);
+	_delay_us(1);
+	
+	if (mode == _4_BIT_MODE)
+	{
+		/* Младший нибл */
+		LCD_PORT |= (1 << EN);
+		LCD_PORT &= 0x0F;
+		LCD_PORT |= (command << 4);
+		_delay_us(1);
+		LCD_PORT &= ~(1 << EN);
+		_delay_us(1);
+	}
 }
 
 void lcd_send_data(unsigned char data)
@@ -20,4 +41,23 @@ void lcd_send_data(unsigned char data)
 void lcd_display_string(unsigned char command, char *string)
 {
 	return;
+}
+
+bool lcd_is_busy(void)
+{
+	unsigned char busy_flag = 0;
+	LCD_PORT &= ~(1 << RS);
+	LCD_PORT |= (1 << RW);
+	_delay_us(1);
+	LCD_PORT |= (1 << EN);
+	_delay_us(1);
+	LCD_PORT &= ~(1 << EN);
+	busy_flag = (LCD_PORT >> 7);
+	_delay_us(1);
+	LCD_PORT |= (1 << EN);
+	_delay_us(1);
+	LCD_PORT &= ~(1 << EN);
+	_delay_us(1);
+	
+	return busy_flag;
 }
