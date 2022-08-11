0d976c5df65ce950631abb1e37a04bcfc6aea7c2
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-08-11 15:37:37 +0400

    Добавил включение lcd_commands, lcd_settings, lcd_util

--- Dual_channel_LPS/inc/mcu_util.h
+++ Dual_channel_LPS/inc/mcu_util.h
@@ -12,6 +12,9 @@
 #include <avr/io.h>
 
 #include "channel_struct.h"
+#include "lcd/lcd_commands.h"
+#include "lcd/lcd_settings.h"
+#include "lcd/lcd_util.h"
 
 void voltage_calculation(Channel* channelX);
 void voltage_display(Channel* channelX);
