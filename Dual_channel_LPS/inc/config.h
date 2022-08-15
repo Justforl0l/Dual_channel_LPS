d158e6f7153e858e8d10f8748fc241a54c82e006
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-08-15 10:47:20 +0400

    Добавил константы
    - Добавил коэффициенты деления и усиления по всем каналам АЦП
    - Добавил величину опорного напряжения АЦП

--- Dual_channel_LPS/inc/config.h
+++ Dual_channel_LPS/inc/config.h
@@ -9,14 +9,23 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-/* Управляющие выводы LCD дисплея */
-#define RS					PD0
-#define RW					PD1
-#define EN					PD2
-#define LCD_PORT			PORTD
-#define LCD_DDR				DDRD
+/* Параметры схемы */
+/* Коэффициенты */
+#define C0_VOLTAGE_DIVIDER_RATIO			10
+#define C1_CURRENT_GAIN						100
+#define C2_VOLTAGE_DIVIDER_RATIO			10
+#define C3_CURRENT_GAIN						100
 
-#define BUTTON_PIN			PB1
-#define LOAD_TOGGLER		PB0
+#define ADC_VOLTAGE_REFERENCE				5000		// Величина опорного напряжения АЦП (в мВ)
+
+/* Управляющие выводы LCD дисплея */
+#define RS									PD0
+#define RW									PD1
+#define EN									PD2
+#define LCD_PORT							PORTD
+#define LCD_DDR								DDRD
+
+#define BUTTON_PIN							PB1
+#define LOAD_TOGGLER						PB0
 
 #endif /* CONFIG_H_ */
