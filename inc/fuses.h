04461e0c4bd90c34cd15d5afe56e0a1c5052756c
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-09-17 13:51:21 +0400

    Добавил фьюзы

---
+++ inc/fuses.h
@@ -0,0 +1,12 @@
+#ifndef FUSES_H_
+#define FUSES_H_
+
+#include "avr/io.h"
+
+FUSES =
+{
+    .low = LFUSE_DEFAULT,
+    .high = (FUSE_BOOTSZ1 & FUSE_BOOTSZ0 & FUSE_WDTON & FUSE_SPIEN)
+};
+
+#endif // FUSES_H_
